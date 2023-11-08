782a8e41a7fd8dcda839dafa403d18fe344f5007
Sameer Bhamla <71896224+sameerbhamla@users.noreply.github.com>
2023-11-08 12:16:57 -0500

    s

--- 6.16Lab/script.js
+++ 6.16Lab/script.js
@@ -18,9 +18,3 @@ function isStrongPassword(password) {
     return true;
   }
   
-  // Test cases
-  console.log(isStrongPassword("Qwerty")); // false - Too short
-  console.log(isStrongPassword("passwordQwerty")); // false - Contains "password"
-  console.log(isStrongPassword("qwerty123")); // false - No uppercase characters
-  console.log(isStrongPassword("Qwerty123")); // true
-  
---
+++ Exam/script.js
@@ -0,0 +1,7 @@
+var name = 
+
+function(name){
+    for( let i = 0; i < 10; i++){
+        console.log(name)
+    }
+}
